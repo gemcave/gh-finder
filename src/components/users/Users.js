bc469cd449c9bdb3767edeb8e145abc1c4dcf85b
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-07-18 13:18:55 +0600

    app was finished

---
+++ src/components/users/Users.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import GithubContext from "../../context/github/githubContext";
+
+export const Users = () => {
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => {
+          const { id, ...userProps } = user;
+          return <UserItem key={id} user={userProps} />;
+        })}
+      </div>
+    );
+  }
+};
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem"
+};
+
+export default Users;
